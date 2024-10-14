731ef7f450668d9bcec3aaa216b4364a750ee491
Alexander <17806067729@163.com>
2024-10-14 22:26:13 +0800

    'a'

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.set_page_config(page_title="Streamlit团队平台", layout="wide")
+
+def main():
+
+    
+    st.write("# 欢迎来到Streamlit和Web页面团队平台")
+    st.write("请从侧边栏选择功能页面")
+
+if __name__ == "__main__":
+    main()
