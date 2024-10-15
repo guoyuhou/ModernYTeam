c61b4750cc62b6190e7b8937cbd8dc81c90595e7
Alexander <17806067729@163.com>
2024-10-15 11:59:16 +0800

    'a'

--- pages/02_学习资源.py
+++ pages/02_学习资源.py
@@ -5,8 +5,17 @@ def read_markdown_file(markdown_file):
     with open(markdown_file, "r", encoding="utf-8") as file:
         return file.read()
 
-st.title("学习资源")
+st.title("编程资源")
 
-introduction_path = os.path.join("content_scripts", "Streamlit.md")
-introduction_content = read_markdown_file(introduction_path)
-st.markdown(introduction_content, unsafe_allow_html=True)
+# 获取"content_scripts/文章"文件夹中的所有Markdown文件
+articles_folder = os.path.join("content_scripts", "编程资源")
+article_files = [f for f in os.listdir(articles_folder) if f.endswith('.md')]
+
+# 创建一个下拉菜单让用户选择文章
+selected_article = st.selectbox("选择一篇文章", article_files)
+
+# 读取并显示选中的文章内容
+if selected_article:
+    article_path = os.path.join(articles_folder, selected_article)
+    article_content = read_markdown_file(article_path)
+    st.markdown(article_content, unsafe_allow_html=True)
