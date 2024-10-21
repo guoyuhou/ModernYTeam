5b3a4c575602678775d458be5a5d7af4dba0d541
Alexander <17806067729@163.com>
2024-10-21 18:10:37 +0800

    'a'

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
-st.set_page_config(page_title="Streamlit团队平台", layout="wide")
+st.set_page_config(page_title="毛登科技", layout="wide")
 
 def main():
 
     
-    st.write("# 欢迎来到Streamlit和Web页面团队平台")
+    st.write("# 欢迎来到毛登科技")
     st.write("请从侧边栏选择功能页面")
 
 if __name__ == "__main__":
